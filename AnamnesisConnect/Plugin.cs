d2c2813e8994aae4cc37a57c6b3709caff29653c
Yuki <yuuki.walsh@outlook.com>
2021-12-23 16:29:05 +1100

    initial connection framework

---
+++ AnamnesisConnect/Plugin.cs
@@ -0,0 +1,100 @@
+﻿// © Customize+.
+// Licensed under the MIT license.
+
+namespace CustomizePlus
+{
+	using System;
+	using System.IO;
+	using System.IO.Pipes;
+	using System.Text;
+	using System.Threading.Tasks;
+	using Dalamud.Game.ClientState;
+	using Dalamud.IoC;
+	using Dalamud.Logging;
+	using Dalamud.Plugin;
+
+	public sealed class Plugin : IDalamudPlugin
+    {
+		private const string PipeName = "AnamnesisConnectPipe";
+
+		private NamedPipeServerStream server;
+		private StreamReader? reader;
+		private StreamWriter? writer;
+
+		private bool loaded = true;
+
+		public Plugin(
+			[RequiredVersion("1.0")] DalamudPluginInterface pluginInterface)
+        {
+			this.PluginInterface = pluginInterface;
+
+			PluginLog.Information("Starting Anamnesis Connect");
+
+			Task.Run(this.Run);
+		}
+
+		public DalamudPluginInterface PluginInterface { get; private set; }
+		public string Name => "Anamnesis Connect";
+
+		public void Send(string message)
+		{
+			if (this.writer == null)
+				return;
+
+			this.writer.WriteLine(message);
+			this.writer.Flush();
+		}
+
+		public void Dispose()
+        {
+			this.loaded = false;
+
+			if (this.server != null)
+			{
+				if (this.server.IsConnected)
+					this.server.Disconnect();
+
+				this.server.Dispose();
+			}
+        }
+
+		private async Task Run()
+		{
+			while (this.loaded)
+			{
+				try
+				{
+					PluginLog.Information("Starting server.");
+					this.server = new(PipeName);
+
+					await this.server.WaitForConnectionAsync();
+
+					this.reader = new StreamReader(this.server);
+					this.writer = new StreamWriter(this.server);
+
+					PluginLog.Information("Server connected");
+
+					this.Send("Hello world");
+
+					while (this.server.IsConnected)
+					{
+						string? message = await this.reader.ReadLineAsync();
+
+						if (message == null)
+							continue;
+
+						PluginLog.Information("Recieved message: " + message);
+					}
+
+					PluginLog.Information("Server disconnected");
+				}
+				catch (Exception ex)
+				{
+					PluginLog.Error(ex, "Anamnesis Connect server error");
+					this.server.Disconnect();
+					this.server.Dispose();
+				}
+			}
+		}
+    }
+}
